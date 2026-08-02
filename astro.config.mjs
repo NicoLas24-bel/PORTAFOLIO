a408b763d22be82ec1a0bfcd0855f18e4b8489ce
Nicolás <nicolasllancaqueo0929@gmail.com>
2026-08-02 18:49:36 -0400

    Configurar despliegue a GitHub Pages con Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://NicoLas24-bel.github.io',
+  base: '/PORTAFOLIO',
   vite: {
     plugins: [tailwindcss()]
   }
